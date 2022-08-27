9fabe25e574a0d2b76909ddc57f449cd5f6406df
Sue Holder <susanpholder@gmail.com>
2022-08-27 16:21:32 +0100

    🚧  add cors &  dependencies

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 
 const campsRouter = require('./routes/camps/camps.router')
 const clubsRouter = require('./routes/clubs/clubs.router')
@@ -10,14 +11,26 @@ const app = express();
 /*
   middleware
 */
+
+const whitelist = ['http://localhost:8080','https://absolutesport.org']
+app.use(cors({
+  origin: function (origin, callback){
+    if (whitelist.indexOf(origin) !== -1){
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 /*
   routes
 */
 
-app.use(campsRouter);
-app.use(clubsRouter);
+app.use('/camps', campsRouter);
+app.use('/clubs', clubsRouter);
+app.use('/parties', partiesRouter);
 
 
 module.exports = app;
